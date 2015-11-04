8db90905c1302355b301a90baa0aff08f8fb5077
Jérémie Foucault <Jeremie@mbp-de-jeremie.home>
2015-11-04 09:31:20 +0100

    Fix PasswordResetServiceProvider

--- src/Jenssegers/Mongodb/Auth/PasswordResetServiceProvider.php
+++ src/Jenssegers/Mongodb/Auth/PasswordResetServiceProvider.php
@@ -1,8 +1,59 @@
 <?php namespace Jenssegers\Mongodb\Auth;
 
 use Jenssegers\Mongodb\Auth\DatabaseTokenRepository as DbRepository;
+use Illuminate\Auth\Passwords\PasswordBroker;
 
 class PasswordResetServiceProvider extends \Illuminate\Auth\Passwords\PasswordResetServiceProvider {
+
+    
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerPasswordBroker();
+
+        $this->registerTokenRepository();
+    }
+
+    /**
+     * Register the password broker instance.
+     *
+     * @return void
+     */
+    protected function registerPasswordBroker()
+    {
+        $this->app->singleton('auth.password', function ($app) {
+            // The password token repository is responsible for storing the email addresses
+            // and password reset tokens. It will be used to verify the tokens are valid
+            // for the given e-mail addresses. We will resolve an implementation here.
+            $tokens = $app['auth.password.tokens'];
+
+            $users = $app['auth']->driver()->getProvider();
+
+            $view = $app['config']['auth.password.email'];
+
+            // The password broker uses a token repository to validate tokens and send user
+            // password e-mails, as well as validating that password reset process as an
+            // aggregate service of sorts providing a convenient interface for resets.
+            return new PasswordBroker(
+                $tokens, $users, $app['mailer'], $view
+            );
+        });
+    }
+
 
     /**
      * Register the token repository implementation.
@@ -24,6 +75,16 @@ class PasswordResetServiceProvider extends \Illuminate\Auth\Passwords\PasswordRe
 
             return new DbRepository($connection, $table, $key, $expire);
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['auth.password', 'auth.password.tokens'];
     }
 
 }
